c3bd913a46386baf1cb31b40dade2b95773530b4
Sandamele <skeithpatrick@gmail.com>
2025-06-07 20:16:08 +0200

    change the rate limit amount

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -15,7 +15,7 @@ import { resetPasswordValidator } from "../validators/auth/resetPasswordValidato
 import { rateLimit } from 'express-rate-limit';
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 15,
+  limit: 10,
   standardHeaders: 'draft-8',
   legacyHeaders: false,
 })
